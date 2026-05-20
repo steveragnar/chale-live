d68e9c932444d4932fd4816ee26b0e86e9492f91
steveragnar <sarfokwekustephen06@gmail.com>
2026-05-20 19:27:17 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+let waitingUser = null;
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  if (waitingUser) {
+    const room = socket.id + '#' + waitingUser.id;
+    socket.join(room);
+    waitingUser.join(room);
+
+    io.to(room).emit('matched', { room, initiator: waitingUser.id });
+    waitingUser = null;
+  } else {
+    waitingUser = socket;
+    socket.emit('waiting');
+  }
+
+  socket.on('message', ({ room, text }) => {
+    socket.to(room).emit('message', { text });
+  });
+
+  // WebRTC signaling
+  socket.on('offer', ({ room, offer }) => {
+    socket.to(room).emit('offer', { offer });
+  });
+
+  socket.on('answer', ({ room, answer }) => {
+    socket.to(room).emit('answer', { answer });
+  });
+
+  socket.on('ice-candidate', ({ room, candidate }) => {
+    socket.to(room).emit('ice-candidate', { candidate });
+  });
+
+  socket.on('disconnect', () => {
+    if (waitingUser === socket) waitingUser = null;
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Chale running on http://localhost:3000');
+});
